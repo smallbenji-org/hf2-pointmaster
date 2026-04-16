6bc6b9e5658950323bb81100c3f515e83a7af3ed
smallbenji <benjaminkfalch@gmail.com>
2026-04-16 14:27:33 +0200

    integrating design guide

--- Pointmaster/js/src/App.vue
+++ Pointmaster/js/src/App.vue
@@ -4,3 +4,40 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style lang="scss">
+:root {
+  --primary: #007bff;
+  --primary-header: #004080;
+  --secondary-1: #f8f9fa;
+  --secondary-2: #e9ecef;
+  --secondary-3: #dee2e6;
+  --secondary-4: #495057;
+  --danger: #ff4d4d;
+
+}
+
+h1 {
+  font-size: 24px;
+  font-weight: 800;
+  color: var(--primary-header)
+}
+
+h2 {
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--primary)
+}
+
+p {
+  font-size: 14px;
+}
+
+.button {
+  padding: 6px;
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  box-shadow: 2px 2px 5px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
--- Pointmaster/js/src/Views/Home.vue
+++ Pointmaster/js/src/Views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
         <h1>Welcome home</h1>
-        <router-link to="/test">
+        <router-link to="/test" class="button">
             Go to test
         </router-link>
     </div>
