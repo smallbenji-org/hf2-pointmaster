32f76f3ce34af3e02e2897e4b9311fd62075ec98
smallbenji <benjaminkfalch@gmail.com>
2026-04-20 12:32:37 +0200

    update topbar

--- Pointmaster/js/src/components/topbar.vue
+++ Pointmaster/js/src/components/topbar.vue
@@ -1,31 +1,42 @@
+<script setup lang="ts">
+import { BNavbar, BNavbarItem } from 'buefy';
+
+</script>
+
 <template>
-    <div class="topbar">
-        <router-link to="/">
-            PointMaster
-        </router-link>
-        <div class="topbar-links">
-            <router-link to="/patruljer">
-                Patruljer
-            </router-link>
-        </div>
-    </div>
+    <b-navbar :shadow="true">
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <h1 class="title">
+                    PointMaster
+                </h1>
+            </b-navbar-item>
+        </template>
+        <template #end>
+            <b-navbar-item tag="div">
+                <router-link to="/patruljer" class="button is-primary">
+                    Patruljer
+                </router-link>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
 </template>
 <style lang="scss">
-.topbar {
-    // border-bottom: 1px solid black;
-    box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.2);
-    padding: 1rem;
-    display: flex;
-    z-index: 1;
-    a {
-        // color: black;
-        text-decoration: none;
-    }
+// .topbar {
+//     // border-bottom: 1px solid black;
+//     box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.2);
+//     padding: 1rem;
+//     display: flex;
+//     z-index: 1;
+//     a {
+//         // color: black;
+//         text-decoration: none;
+//     }
 
-    &-links {
-        margin-left: auto;
-        display: flex;
-        gap: 5px;
-    }
-}
+//     &-links {
+//         margin-left: auto;
+//         display: flex;
+//         gap: 5px;
+//     }
+// }
 </style>
