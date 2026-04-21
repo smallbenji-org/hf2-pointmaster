8f8e6f387861218ea6771ea68114f8fc6de4d96c
smallbenji <benjaminkfalch@gmail.com>
2026-04-21 14:49:24 +0200

    adding postgresql integration for points

--- Pointmaster/Repositories/PointRepository.cs
+++ Pointmaster/Repositories/PointRepository.cs
@@ -1,3 +1,8 @@
+using System.Data;
+using Dapper;
+using Microsoft.Extensions.Options;
+using Npgsql;
+
 namespace Pointmaster.Repositories
 {
     public interface IPointRepository
@@ -7,6 +12,7 @@ namespace Pointmaster.Repositories
         Task<Point> GetPointById(int Id);
         Task<List<Point>> GetPointByPatrulje(int PatruljeId);
         Task<List<Point>> GetAll();
+        Task DeletePoint(int Id);
     }
 
     public class DummyPointRepository : IPointRepository
@@ -39,6 +45,90 @@ namespace Pointmaster.Repositories
         public async Task<List<Point>> GetAll()
         {
             return _points;
+        }
+
+        public async Task DeletePoint(int Id)
+        {
+            var point = _points.FirstOrDefault(x => x.Id == Id);
+            _points.Remove(point);
+        }
+    }
+    public class PointRepository : IPointRepository
+    {
+        private readonly IOptions<PointMasterConfig> options;
+        private readonly IPatruljeRepository patruljeRepository;
+        private readonly IPostRepository postRepository;
+
+        public PointRepository(IOptions<PointMasterConfig> options, IPatruljeRepository patruljeRepository, IPostRepository postRepository)
+        {
+            this.options = options;
+            this.patruljeRepository = patruljeRepository;
+            this.postRepository = postRepository;
+        }
+
+        private IDbConnection db => new NpgsqlConnection(options.Value.ConnectionString);
+
+        public async Task AddPoint(Point point)
+        {
+            const string sql = @"
+                INSERT INTO points (points, turnout, patrulje_id, post_id)
+                VALUES (@point, @turnout, @patruljeId, @postId)
+            ";
+            using var conn = db;
+            await db.ExecuteAsync(sql, new { point = point.Points, turnout = point.Turnout, patruljeId = point.Patrulje.Id, postId = point.Post.Id });
+        }
+
+        public async Task AddPointRange(List<Point> points)
+        {
+            foreach (var point in points)
+            {
+                await AddPoint(point);
+            }
+        }
+
+        public async Task DeletePoint(int Id)
+        {
+            const string sql = @"
+                DELETE FROM points
+                WHERE id = @id
+            ";
+            using var conn = db;
+            await db.ExecuteAsync(sql, new { id = Id });
+        }
+
+        public async Task<List<Point>> GetAll()
+        {
+            const string sql = @"
+                SELECT
+                    *
+                FROM points
+            ";
+            using var conn = db;
+            return (await db.QueryAsync<Point>(sql)).ToList();
+        }
+
+        public async Task<Point> GetPointById(int Id)
+        {
+            const string sql = @"
+                SELECT
+                    *
+                FROM points
+                WHERE id = @id
+            ";
+            using var conn = db;
+            return await db.QueryFirstAsync<Point>(sql);
+        }
+
+        public async Task<List<Point>> GetPointByPatrulje(int PatruljeId)
+        {
+            const string sql = @"
+                SELECT
+                    *
+                FROM points
+                WHERE patrulje_id = @id
+            ";
+            using var conn = db;
+            return (await db.QueryAsync<Point>(sql, new { id = PatruljeId })).ToList();
         }
     }
 }
