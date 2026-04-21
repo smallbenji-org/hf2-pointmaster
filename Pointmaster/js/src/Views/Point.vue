c8c456953fa343bc30ed3b11e8d7969cfa05352d
smallbenji <benjaminkfalch@gmail.com>
2026-04-21 11:39:30 +0200

    Point W.I.P

--- Pointmaster/js/src/Views/Point.vue
+++ Pointmaster/js/src/Views/Point.vue
@@ -1,7 +1,49 @@
 <template>
-    <div>
-        <h1 class="title">Point</h1>
+    <div class="point">
+        <div class="container point-topbar">
+            <h1 class="title is-4">
+                Point
+            </h1>
+            <div class="actions">
+                <BButton type="is-warning">Opret Point</BButton>
+            </div>
+        </div>
+        <div class="container">
+            <BTable :data="POINT" :columns="columns"></BTable>
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
+import { usePointStore } from '@/Modules/PointModule';
+import { BButton, BTable } from 'buefy';
+import { storeToRefs } from 'pinia';
+
+const pointStore = usePointStore();
+const { POINT } = storeToRefs(pointStore);
+
+const columns = [
+    {
+        field: "id",
+        label: "ID"
+    },
+    {
+        field: "name",
+        label: "Navn"
+    }
+];
+
 </script>
+<style lang="scss">
+.point {
+    &-topbar {
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+        align-items: center;
+
+        h1 {
+            margin-bottom: 0 !important;
+        }
+    }
+}
+</style>
